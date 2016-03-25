907192e2559ea251ac3af18066f30064785335a0
Beto23 <josee-45@hotmail.com>
2016-03-24 23:16:54 -0600

    added event to controller

--- app/js/controllers/Event.controller.js
+++ app/js/controllers/Event.controller.js
@@ -5,4 +5,9 @@ EventController.Inject=['$scope']
 
 function EventController ($scope){
   console.log('EventController');
+  $scope.event = {
+    name: 'Angular Boot Camp',
+    date: '24/03/2016',
+    time: '11:23 am'
+  }
 }

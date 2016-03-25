ef65d9ec82f3dff345b32478f6352010954b3c48
Beto23 <josee-45@hotmail.com>
2016-03-25 00:19:32 -0600

    added votes for the sessions

--- app/js/controllers/Event.controller.js
+++ app/js/controllers/Event.controller.js
@@ -21,21 +21,24 @@ function EventController ($scope){
         creatorName: 'Bob Smith',
         duration: '1hr',
         level: 'Advanced',
-        abstract: 'In this session you will learn the ins and outs of directives!'
+        abstract: 'In this session you will learn the ins and outs of directives!',
+        upVoteCount: 0
       },
       {
         name: 'Scope for fun and profit',
         creatorName: 'Jhon Doe',
         duration: '30mins',
         level: 'Introductory',
-        abstract: 'This session will take a closer look at Scope. Learn what they do, how they do it, and how to get them to do it for you'
+        abstract: 'This session will take a closer look at Scope. Learn what they do, how they do it, and how to get them to do it for you',
+        upVoteCount: 0
       },
       {
         name: 'Well Behaved Controllers',
         creatorName: 'Jhon Doe',
         duration: '2 hours',
         level: 'Intermediate',
-        abstract: 'Controllers are the beginning of everything Angular does Learn how to craf controllers that will win the repect of your friends and neighbors'
+        abstract: 'Controllers are the beginning of everything Angular does Learn how to craf controllers that will win the repect of your friends and neighbors',
+        upVoteCount: 0
       }
     ]
 

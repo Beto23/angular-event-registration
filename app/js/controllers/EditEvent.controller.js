a324d8629e4f12fba60e5275c81b7a1b418a6dd4
Beto23 <josee-45@hotmail.com>
2016-03-26 19:28:30 -0600

    added validation

--- app/js/controllers/EditEvent.controller.js
+++ app/js/controllers/EditEvent.controller.js
@@ -4,8 +4,10 @@ eventsApp.controller('EditEventController', EditEventController)
 EditEventController.inject = ['$scope']
 
 function EditEventController ($scope){
-  $scope.saveEvent = function (event) {
-    window.alert('event ' + event.name + ' saved');
+  $scope.saveEvent = function (event, newEventForm) {
+    if (newEventForm.$valid) {
+      window.alert('event ' + event.name + ' saved');
+    }
   }
 
   $scope.cancelEdit = function (){
